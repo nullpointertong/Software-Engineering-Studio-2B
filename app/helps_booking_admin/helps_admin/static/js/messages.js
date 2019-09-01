f7148f713c980a5edb2daea009e8bfeca6d76804
Wren71 <wrenfajardo123@gmailc.com>
2019-09-01 20:54:43 +1000

    work on messages page admin

---
+++ app/helps_booking_admin/helps_admin/static/js/messages.js
@@ -0,0 +1,14 @@
+function disableTxt() {
+    document.getElementById("text1").disabled = true;
+    document.getElementById("text2").disabled = true;
+    document.getElementById("textheading1").disabled = false;
+    document.getElementById("textheading2").disabled = false;
+
+  }
+  
+  function undisableTxt() {
+    document.getElementById("text1").disabled = false;
+    document.getElementById("text2").disabled = false;
+    document.getElementById("textheading1").disabled = false;
+    document.getElementById("textheading2").disabled = false;
+  }
