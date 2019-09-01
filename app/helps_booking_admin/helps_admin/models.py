a4e308b352078c0d1c4f43d91965f4e6dd8a7471
unknown <auz.richardt@gmail.com>
2019-09-01 19:22:44 +1000

    Basic Migrations

--- app/helps_booking_admin/helps_admin/models.py
+++ app/helps_booking_admin/helps_admin/models.py
@@ -113,6 +113,7 @@ class Workshop(models.Model):
     students = models.ManyToManyField('StudentAccount')
     max_students = models.PositiveIntegerField()
     skill_set_name = models.CharField(max_length=64)
+    workshop_files = models.FileField()
     # start_dates = DateListField()
     # end_dates = DateListField()
     # Each workshop should be a separate entity, regardless of whether they are the same skillset
