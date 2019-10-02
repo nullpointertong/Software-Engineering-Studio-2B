ec2f041a108c9075a1e16304ba10bb6b855e6105
Wren71 <wrenfajardo123@gmailc.com>
2019-10-02 18:45:17 +1000

    Messages form validation and getting values from inputs

--- app/helps_booking_admin/helps_admin/static/js/messages.js
+++ app/helps_booking_admin/helps_admin/static/js/messages.js
@@ -1,13 +1,13 @@
 function disableTxt() {
     document.getElementById("text1").disabled = true;
     document.getElementById("text2").disabled = true;
-    document.getElementById("textheading1").disabled = false;
-    document.getElementById("textheading2").disabled = false;
+    document.getElementById("textheading1").disabled = true;
+    document.getElementById("textheading2").disabled = true;
 
   }
   
   function undisableTxt() {
-    document.getElementById("text1").disabled = false;
+    document.getElementById("textarea").disabled = false;
     document.getElementById("text2").disabled = false;
     document.getElementById("textheading1").disabled = false;
     document.getElementById("textheading2").disabled = false;
